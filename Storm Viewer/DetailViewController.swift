51668017221283c7d0e13a475fca9c821c30a669
Forrest Parker <forrestwparker@gmail.com>
2018-04-16 14:27:59 -0700

    Completed Section 5: Loading images with UIImage

--- Storm Viewer/DetailViewController.swift
+++ Storm Viewer/DetailViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    
     @IBOutlet var imageView: UIImageView!
+    
+    var selectedImage: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        if let imageToLoad = selectedImage {
+            imageView.image  = UIImage(named: imageToLoad)
+        }
     }
 
     override func didReceiveMemoryWarning() {
