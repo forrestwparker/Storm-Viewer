89073566af82addd0e38adfec30103a5cdde6f62
Forrest Parker <forrestwparker@gmail.com>
2018-04-18 08:41:20 -0700

    Completed Section 2: UIActivityViewController explained

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Storm Viewer
 My implementation of [Project 1: Storm Viewer](https://www.hackingwithswift.com/read/1/overview) from the [Hacking With Swift](https://www.hackingwithswift.com/) website.
+
+Modified by [Project 3: Social Media](https://www.hackingwithswift.com/read/3/overview).
--- Storm Viewer/DetailViewController.swift
+++ Storm Viewer/DetailViewController.swift
@@ -19,6 +19,7 @@ class DetailViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         title = selectedImage
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
         navigationItem.largeTitleDisplayMode = .never
         if let imageToLoad = selectedImage {
             imageView.image  = UIImage(named: imageToLoad)
@@ -33,6 +34,12 @@ class DetailViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.hidesBarsOnTap = false
+    }
+    
+    @objc func shareTapped() {
+        let vc = UIActivityViewController(activityItems: [imageView.image!], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
     }
     
     override func didReceiveMemoryWarning() {
